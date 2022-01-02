380d5c68fcb28e1760e5001358b86d071811cc3d
Basteque <82060824+Basteque@users.noreply.github.com>
2022-01-02 10:39:23 +0100

    Co-authored-by: RayanBGD <RayanBGD@users.noreply.github.com> Co-authored-by: doriadrm <doriadrm@users.noreply.github.com>

--- request.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'pexels';
-const img = document.getElementById('fetch');
-
-const client = createClient('563492ad6f917000010000016ac120d8cff147a5a2b79a1cf44c40d6');
-const query = 'Nature';
-
-client.photos.search({ query, per_page: 1 }).then(photos => console.log(photos));
